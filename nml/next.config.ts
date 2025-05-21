166b5ed1b045a798dd47379d99310540688ef0d2
WillemBlokland <97394592+WillemBlokland@users.noreply.github.com>
2025-05-22 01:26:06 +0200

    Delete nml directory

--- nml/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['libraryofshortstories.s3.ap-southeast-2.amazonaws.com'],
-  },
-};
-
-export default nextConfig;
--- nml/src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-// srs/app/about.tsx
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>Communication and Media</h1>
-      <p>Find some information.</p>
-    </div>
-  );
-}
