61b125534a830f21814acb952ada32232f2627da
Willem Blokland <wb.blokland@gmail.com>
2025-05-22 14:34:46 +0200

    Added Flask cors

--- NML-Frontend-main/backend/ai_component.py
+++ NML-Frontend-main/backend/ai_component.py
@@ -4,6 +4,14 @@ import openai
 import pandas as pd
 from dotenv import load_dotenv
 
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=["https://nm-lfinal-beta.vercel.app"])
+
+
 app = Flask(__name__)
 load_dotenv()
 
